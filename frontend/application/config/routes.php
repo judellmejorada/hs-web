573626f69a79cea327252af1f1a2d360f32410fe
mrish08 <101469822+mrish08@users.noreply.github.com>
2022-06-26 20:33:35 +0800

    Update routes.php

--- frontend/application/config/routes.php
+++ frontend/application/config/routes.php
@@ -55,7 +55,8 @@ $route['default_controller'] = 'landing';
 $route['login'] = 'Access/login';
 $route['register'] = 'Register/RegisterPage';
 $route['service'] = 'service/ServicePage';
-
+$route['aboutus']='aboutus';
+$route['faaq']='faaq';
 
 //Admin Files
 $route['admin'] = 'Admin_SystemSetup/UI';
