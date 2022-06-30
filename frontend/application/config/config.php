66b50ec43769bca6cb99c286534f52919daa9851
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-06-29 14:26:01 -1100

    Added some lines -- index.php

--- frontend/application/config/config.php
+++ frontend/application/config/config.php
@@ -380,8 +380,8 @@ $config['encryption_key'] = '';
 |
 */
 $config['sess_driver'] = 'files';
-$config['sess_cookie_name'] = 'ci_session';
-$config['sess_expiration'] = 7200;
+$config['sess_cookie_name'] = '_'.sha1(date('Y-m-d'));
+$config['sess_expiration'] = time() + 1560000;
 $config['sess_save_path'] = NULL;
 $config['sess_match_ip'] = FALSE;
 $config['sess_time_to_update'] = 300;
