0b810fa15adc906c5658336e85680729b9c109c4
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-07-10 12:24:39 -1100

    parsley

--- application/views/access/pages-login.php
+++ application/views/access/pages-login.php
@@ -57,11 +57,14 @@
                                         <a href="pages-recoverpw.php" class="text-muted float-end"><small>Forgot your password?</small></a>
                                         <label for="password" class="form-label">Password</label>
                                         <div class="input-group input-group-merge">
-                                            <input type="password" id="password" name="password" class="form-control" required="" placeholder="Enter your password" 
-                                                data-parsley-required = "true">
+                                            <input type="password" id="password" name="password" class="form-control" required="" placeholder="Enter your password" >
                                             <div class="input-group-text" data-password="false">
                                                 <span class="password-eye"></span>
                                             </div>
+                                            <div class="invalid-feedback">
+                                                Please Provide you password correctly
+                                            </div>
+                                        </div>
                                         </div>
                                     </div>
 
