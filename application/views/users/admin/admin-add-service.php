57bd33cd64b43b045ea8b7d69d6119eeb8bd0651
Jarret <Jarretbenedict@gmail.com>
2022-07-12 12:01:49 +0800

    test

--- application/views/users/admin/admin-add-service.php
+++ application/views/users/admin/admin-add-service.php
@@ -12,41 +12,25 @@
                     <div class="row g-3">
                         <div class="mb-3 col-md-6">
                             <label class="form-label" for="validationCustom01">Service Name</label>
-                            <input type="text" class="form-control" id="validationCustom00" placeholder="Service Name" required="">
+                            <input type="text" class="form-control" id="validationCustom01" placeholder="Service Name" required="">
                             <div class="invalid-feedback">
                                 Please provide a Service Name.
                             </div>
                         </div>
                         <div class="mb-3 col-md-6">
-                                <label class="form-label" for="validationCustom01">Service Image</label>
-                                <input type="file" id="validationCustom01" class="form-control" required>
-                                <div class="invalid-feedback">
-                                        Please provide an Service Image. 
-                                </div>
+                            <label class="form-label" for="validationCustom02">Service Image</label>
+                            <input type="file" id="validationCustom02" class="form-control" required="">
+                            <div class="invalid-feedback">
+                                Please provide an Service Image. 
                             </div>
+                        </div>
                     </div>
                     
                     <div class="row g-2">
-                        <div id="snow-editor" style="height: 300px;">
-                        <h3><span class="ql-size-large">Hello World!</span></h3>
-                        <p><br></p>
-                        <h3>This is an simple editable area.</h3>
-                        <p><br></p>
-                        <ul>
-                            <li>
-                                Select a text to reveal the toolbar.
-                            </li>
-                            <li>
-                                Edit rich document on-the-fly, so elastic!
-                            </li>
-                        </ul>
-                        <p><br></p>
-                        <p>
-                            End of simple area
-                        </p>
-                            <div class="invalid-feedback">
-                                    Please provide a brief Description.  
-                            </div>
+                        <label class="form-label" for="validationCustom03">Description</label>
+                        <div id="snow-editor" style="height: 300px;" required="" ></div>
+                        <div class="invalid-feedback">
+                                Please provide a brief Description.  
                         </div>
                     </div>
                     <br>
