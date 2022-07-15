12eee8097d3cd48729aa954ec42982226f5ca99e
Jarret <Jarretbenedict@gmail.com>
2022-07-15 14:59:50 +0800

    test

--- application/views/access/pages-login.php
+++ application/views/access/pages-login.php
@@ -78,7 +78,7 @@
                                                 <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-facebook"></i></a>
                                             </li>
                                             <li class="list-inline-item">
-                                                <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="g-signin"></i></a>
+                                                <a href="javascript: void(0);" class="g-signin2"><i class="mdi mdi-google"></i></a>
                                             </li>
                                             
                                             <li class="list-inline-item">
