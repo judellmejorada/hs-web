4c738577d7ccb032b275821800dd9507b3149ac1
Jarret <Jarretbenedict@gmail.com>
2022-07-15 14:34:06 +0800

    test

--- application/views/access/pages-login.php
+++ application/views/access/pages-login.php
@@ -17,10 +17,6 @@
         <link href="<?php echo base_url('assets')?>/css/app.min.css" rel="stylesheet" type="text/css" id="light-style" />
         <link href="<?php echo base_url('assets')?>/css/app-dark.min.css" rel="stylesheet" type="text/css" id="dark-style" />
 
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" 
-        crossorigin="anonymous">
-
         <!-- Parsley css -->
         <link href="<?php echo base_url('assets')?>/css/parsley.css" rel="stylesheet" type="text/css"/>
 
@@ -82,7 +78,7 @@
                                                 <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-facebook"></i></a>
                                             </li>
                                             <li class="list-inline-item">
-                                                <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
+                                                <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google" data-onsuccess="onSignIn"></i></a>
                                             </li>
                                             <li class="list-inline-item">
                                                 <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
@@ -128,10 +124,6 @@
         <!-- bundle -->
         <script src="<?php echo base_url('assets')?>/js/vendor.min.js"></script>
         <script src="<?php echo base_url('assets')?>/js/app.min.js"></script>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" 
-        integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" 
-        crossorigin="anonymous"></script>
 
         <!-- parsley -->
         <script src="<?php echo base_url('assets')?>/js/parsley.js"></script>
--- assets/js/index.js
+++ assets/js/index.js
@@ -5,3 +5,14 @@ function onSignIn(googleUser) {
     $("#users_profile_pic").attr('src', getUsers_profile_picUrl());
 
   }
+
+function signOut() {
+  var auth2 = gapi.auth2.getAuthInstance();
+  auth2.signOut().then(function(){
+    alert("You have been signed out successfully");
+    $(".g-signin2").css('display', 'block');
+    $(".data").css('display', 'none');
+
+  });
+
+}
