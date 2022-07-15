29ea8abebf0dc7ce8df3dd0144f54a0b76b8b527
Jarret <Jarretbenedict@gmail.com>
2022-07-15 16:02:27 +0800

    test

--- application/views/access/pages-login.php
+++ application/views/access/pages-login.php
@@ -74,18 +74,11 @@
                                     <div class="text-center mt-4">
                                         <p class="text-muted font-16">Sign in with</p>
                                         <ul class="social-list list-inline mt-3">
+
                                             <li class="list-inline-item">
-                                                <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-facebook"></i></a>
+                                                <i class="g-signin2" data-width="100" data-height="50" data-longtitle="true"></i></a>
                                             </li>
-                                            <li class="list-inline-item">
-                                                <i class="g-signin2" data-width="30" data-height="30" data-longtitle="false"></i></a>
-                                            </li>
-                                            <li class="list-inline-item">
-                                                <a href="javascript: void(0);" id="nobackground" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
-                                            </li>
-                                            <li class="list-inline-item">
-                                                <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github"></i></a>
-                                            </li>
+                                            
                                         </ul>
                                     </div>
 
