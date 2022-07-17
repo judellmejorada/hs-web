59f4ecfa577400f0c8d47ecf75e09b422a11953b
mrish08 <101469822+mrish08@users.noreply.github.com>
2022-07-17 14:41:36 +0800

    Modified route

--- application/controllers/Dentist.php
+++ application/controllers/Dentist.php
@@ -1,7 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Dentist_Calendar extends CI_Controller {
+class Dentist_SystemSetup extends CI_Controller {
 
 	/**
 	 * Index Page for this controller.
@@ -18,7 +18,7 @@ class Dentist_Calendar extends CI_Controller {
 	 * map to /index.php/welcome/<method_name>
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
-	public function CalendarPage()
+	public function UI()
 	{
 		$this->load->view('users/dentists/dentists-calendar');
 		$this->load->view('partials/dentist/top_bar');
