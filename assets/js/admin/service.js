378d6e0822c7f3616380dc15ba909a19c81c51e5
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-07-19 22:47:06 +0800

    display datatable

---
+++ assets/js/admin/service.js
@@ -0,0 +1,73 @@
+$(function () {
+	const dataTable = $("#service-datatable").DataTable(
+		getDataTableConfig({
+			ajax: getAjaxConfig("/admin/service/get-all-services", {
+				type: "GET",
+			}),
+			columns: [
+				{
+					data: null,
+					render: function (data, type, row, meta) {
+						return `<img src="${baseURLUserProfile}/${data.services_image}" alt="table-user" class="me-2 rounded-circle"><a href="javascript:void(0);" class="text-body fw-semibold">${data.services_name}</a>`;
+					},
+					className: "table-user",
+				},
+				{
+					// birth day not available
+					data: "services_description",
+					render: function (data, type, row, meta) {
+						return `<span>${data.substring(0, 50) + "..."}</span>`;
+					},
+				},
+				{
+					data: null,
+					render: function (data, type, row, meta) {
+						return "N/A";
+					},
+				},
+				{
+					data: null,
+					render: function (data, type, row, meta) {
+						return "N/A";
+					},
+				},
+				{
+					data: "services_created_at",
+				},
+				{
+					data: "services_status",
+					render: function (data, type, row, meta) {
+						return `<span class="badge badge-danger-lighten">${data}</span>`;
+					},
+				},
+				{
+					orderable: !1,
+					data: null,
+					render: function () {
+						return (
+							`<a href="javascript:void(0);" class="action-icon" data-bs-toggle="modal" data-bs-target="#staticBackdrop1"> <i class="mdi mdi-account-outline" ></i></a>` +
+							`<a href="javascript:void(0);" class="action-icon" data-bs-toggle="modal" data-bs-target="#staticBackdrop2"> <i class="mdi mdi-square-edit-outline"></i></a>` +
+							`<a href="javascript:void(0);" class="action-icon" data-bs-toggle="modal" data-bs-target="#staticBackdrop3"> <i class="mdi mdi-delete"></i></a>`
+						);
+					},
+				},
+			],
+		})
+	);
+
+	$(document).on("submit", "#add-user-form", async function (event) {
+		event.preventDefault();
+		let formData = new FormData(this);
+		await $.ajax(
+			getAjaxConfig("/admin/user/add-users", {
+				type: "POST",
+				data: formData,
+				contentType: false,
+				processData: false,
+			})
+		);
+		$("#staticBackdrop0").modal("toggle");
+		dataTable.ajax.reload();
+		return false;
+	});
+});
