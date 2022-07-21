7777f57f20606560969f699da6ff564a98ca479d
Jarret <Jarretbenedict@gmail.com>
2022-07-21 20:08:04 +0800

    test

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -54,10 +54,11 @@ class Register extends CI_Controller {
 				$this->db->insert('Users', $data);
 
 				$this->session->set_flashdata("success", "Your Account has been registered. You can login now!");
-				redirect(base_url('register', "refresh"));
-			}
+				redirect('Register/RegisterPage', 'refresh');
+			
 		}
 		$this->session->sess_destroy();
 		$this->load->view('access/pages-register');
 	}
+}
 }
