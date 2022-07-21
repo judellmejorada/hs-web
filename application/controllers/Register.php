3e74e55147cab00c8b6441d0dec59ecf35df6537
Jarret <Jarretbenedict@gmail.com>
2022-07-21 18:21:04 +0800

    test

--- application/controllers/Register.php
+++ application/controllers/Register.php
@@ -21,16 +21,16 @@ class Register extends CI_Controller {
 	public function RegisterPage()
 	{
 		if (ISSET ($_POST['submit'])){
-			$this->form_validation->set_rules('users_fname', 'Users_fname', 'required');
-			$this->form_validation->set_rules('users_mname', 'Users_mname', '');
-			$this->form_validation->set_rules('users_lname', 'users_lname', 'required');
-			$this->form_validation->set_rules('users_birthdate', 'Users_birthdate', 'required');
-			$this->form_validation->set_rules('users_phone_number', 'Users_phone_number', 'required|min_length[5]');
-			$this->form_validation->set_rules('users_civil_status', 'Users_civil_status', 'required');
-			$this->form_validation->set_rules('users_gender', 'Users_gender', 'required');
-			$this->form_validation->set_rules('users_profile_pic', 'Users_profile_pic', '');
-			$this->form_validation->set_rules('users_email', 'Users_email', 'required');
-			$this->form_validation->set_rules('users_password', 'Users_password', 'required|min_length[5]');
+			$this->form_validation->set_rules('users_fname', 'First Name', 'required');
+			$this->form_validation->set_rules('users_mname', 'Middle Name', '');
+			$this->form_validation->set_rules('users_lname', 'Last Name', 'required');
+			$this->form_validation->set_rules('users_birthdate', 'Birth Date', 'required');
+			$this->form_validation->set_rules('users_phone_number', 'Phone Number', 'required|min_length[5]');
+			$this->form_validation->set_rules('users_civil_status', 'Civil Status', 'required');
+			$this->form_validation->set_rules('users_gender', 'Gender', 'required');
+			$this->form_validation->set_rules('users_profile_pic', 'Profile Picture', 'required');
+			$this->form_validation->set_rules('users_email', 'Email', 'required');
+			$this->form_validation->set_rules('users_password', 'Password', 'required|min_length[5]');
 			$this->form_validation->set_rules('users_password', 'Confirm Password', 'required|min_length[5]|matches[users_password]');
 			
 			//If form validation is true
