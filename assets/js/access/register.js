0000a0b11ef08a88776d338ea1bae47532e97e88
Jarret <Jarretbenedict@gmail.com>
2022-07-22 17:16:46 +0800

    test

--- assets/js/access/register.js
+++ assets/js/access/register.js
@@ -1,30 +1,29 @@
 $(function () {
    $(document).on("submit", "#form_register", async function (event) {
     event.preventDefault();
-    let formData = new FormData(this);
-    
-    if ($("#form_register").parsley().validate()) {
-    
-        await $.ajax(
-            getAjaxConfig("/register", {
-                type: "POST",
-                data: formData,
-                contentType: false,
-                processData: false,
 
-                // If registration is successful
-                success: function ({ responseJSON }) {
-                    console.log(responseJSON);
-                    notification("success", "", responseJSON.message.join());
-                },
-                error: function ({ responseJSON }) {
-                    console.log(responseJSON);
-                    notification("error", "", responseJSON.message.join());
-                },
-            })
-        );
-        $("#form_register")[0].reset();
-    };
+    let formData = new FormData(this);
+
+    await $.ajax(
+        getAjaxConfig("/register", {
+            async: true,
+            type: "POST",
+            data: formData,
+            contentType: false,
+            processData: false,
+
+            // If registration is successful
+            success: function ({ responseJSON }) {
+                console.log(responseJSON);
+                notification("success", "", responseJSON.message);
+            },
+            error: function ({ responseJSON }) {
+                console.log(responseJSON);
+                notification("error", "", responseJSON.message);
+            },
+        })
+    );
+    $("#form_register")[0].reset();
 });
 
 });
