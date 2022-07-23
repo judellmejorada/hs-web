0c511cdf56ef6b27342b481ba4c3d90a7ccd2d5c
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-07-23 04:05:06 -1100

    add appointment form

--- application/views/users/patients/patient-add-appointment.php
+++ application/views/users/patients/patient-add-appointment.php
@@ -8,62 +8,63 @@
             </div> <!-- end modal header -->
             <div class="modal-body">
 
-            <form class="needs-validation" novalidate="">
-            <div class="row g-2">
-                    <div class="mb-3 col-md-6">
-                        <label class="form-label" for="validationCustom00">Branch</label>
-                        <select class="form-select" id="validationCustom00" required="">
-                            <option></option>
-                            <option>Kendall</option>
-                            <option>Nadine</option>
-                            <option>Barbie</option>
-                            <option>Kylie</option>
-                        </select>
-                        <div class="invalid-feedback">
-                            Please choose a Dentist.
-                        </div>
-                    </div>
-                    <div class="mb-3 col-md-6">
-                        <!-- This is from schedule available -->
-                        <label class="form-label" for="validationCustom00">Available Schedule</label>
-                        <select class="form-select" id="validationCustom00" required="">
-                            <option></option>
-                            <option>Anne Hathaway - July 28, 2022 8AM</option>
-                            <option>Anne Hathaway - July 28, 2022 9AM</option>
-                            <option>Lora Joy - July 28, 2022 11AM</option>
-                        </select>
-                        <div class="invalid-feedback">
-                            Please choose a Schedule.
-                        </div>
-                    </div>
-                </div>
-                </div>                
+                <form class="needs-validation" novalidate="">
                     <div class="row g-2">
-                            <div class="mb-3 col-md-6">
-                                <div class="form-floating">
-                                    <textarea class="form-control" placeholder="Description" id="floatingTextarea" required="" style="height: 100px;"></textarea>
-                                    <label for="floatingTextarea">Purpose</label>
-                                    <div class="invalid-feedback">
-                                        Please provide a purpose.  
-                                    </div>
-                                </div>
+                        <div class="mb-3 col-md-6">
+                            <label class="form-label" for="validationCustom00">Branch</label>
+                            <select class="form-select" id="validationCustom00" required="">
+                                <option></option>
+                                <option>Kendall</option>
+                                <option>Nadine</option>
+                                <option>Barbie</option>
+                                <option>Kylie</option>
+                            </select>
+                            <div class="invalid-feedback">
+                                Please choose a Dentist.
                             </div>
-                        </div>  
-                        <br>
-                        <div class="mb-3">
-                            <div class="form-check">
-                                <input type="checkbox" class="form-check-input" id="invalidCheck" required="">
-                                <label class="form-check-label form-label" for="invalidCheck">I confirm that the details that I have 
-                                provided in this Personal Information are correct, <br> and that I have not deliberately withheld any relevant information. </label>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <!-- This is from schedule available -->
+                            <label class="form-label" for="validationCustom00">Available Schedule</label>
+                            <select class="form-select" id="validationCustom00" required="">
+                                <option></option>
+                                <option>Anne Hathaway - July 28, 2022 8AM</option>
+                                <option>Anne Hathaway - July 28, 2022 9AM</option>
+                                <option>Lora Joy - July 28, 2022 11AM</option>
+                            </select>
+                            <div class="invalid-feedback">
+                                Please choose a Schedule.
+                            </div>
+                        </div>
+                    </div>
+
+                    <br>        
+                    <div class="row g-2">
+                        <div class="mb-3 col-md-12">
+                            <div class="form-floating">
+                                <textarea class="form-control" placeholder="Description" id="floatingTextarea" required="" style="height: 100px;"></textarea>
+                                <label for="floatingTextarea">Purpose</label>
                                 <div class="invalid-feedback">
-                                    You must agree before submitting.
+                                    Please provide a purpose.  
                                 </div>
                             </div>
                         </div>
-                        <div class="modal-footer">
-                    <button class="btn btn-primary" type="submit">Submit form</button>
-                </form>
+                    </div>  
+                    <br>
+                    <div class="mb-3">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="invalidCheck" required="">
+                            <label class="form-check-label form-label" for="invalidCheck">I confirm that the details that I have 
+                            provided in this Personal Information are correct, <br> and that I have not deliberately withheld any relevant information. </label>
+                            <div class="invalid-feedback">
+                                You must agree before submitting.
+                            </div>
                         </div>
+                    </div>
+                        <div class="modal-footer">
+                        <button class="btn btn-primary" type="submit">Submit form</button>
+                </form>
+                </div>
             </div>
             </div> <!-- end modal footer -->
         </div> <!-- end modal content-->
