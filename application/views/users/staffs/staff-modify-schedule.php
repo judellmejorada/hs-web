196cffba65acc984eecda781502b93011984b099
Jarret <Jarretbenedict@gmail.com>
2022-07-29 23:48:37 +0800

    test

--- application/views/users/staffs/staff-modify-schedule.php
+++ application/views/users/staffs/staff-modify-schedule.php
@@ -1,85 +1,67 @@
 ﻿<!-- Modal -->
-<div class="modal fade" id="staticBackdrop10" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+<div class="modal fade" id="staticBackdrop9" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
     <div class="modal-dialog modal-lg">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title" id="staticBackdropLabel10">Modify Appointment Information</h5>
+                <h5 class="modal-title" id="staticBackdropLabel9">Add Appointment Information</h5>
                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-hidden="true"></button>
             </div> <!-- end modal header -->
             <div class="modal-body">
 
-            <form id="edit-appointment-form" class="needs-validation" novalidate="">
-                <input type="hidden" id="appointments_id" name="appointments_id">
+            <form id="add-appointment-form" class="needs-validation" novalidate="">
             <div class="row g-2">
                             <div class="mb-3 col-md-6">
-                                <label class="form-label" for="branches_name">Branch</label>
-                                <select class="form-select" id="edit_appointments_branch" name="appointments_branch" placeholder="Branch" required="">
+                                <label class="form-label" for="schedule_dentist">Dentist</label>
+                                <select class="form-select" id="schedule_dentist" name="schedule_dentist" placeholder="Branch" required="">
                                     <option></option>
-                                    <option value="Commonwealth">Commonwealth</option>
-                                    <option value="Quezon City">Quezon City</option>
-                                    <option value="Barbie">Barbie</option>
-                                    <option value="Kylie">Kylie</option>
+                                    <option>Dr. Stark</option>
+                                    <option>Dr. McStuffins</option>
+                                    <option>Dr. Quack</option>
+                                    <option>Dr. Dora</option>
                                 </select>
                                 <div class="invalid-feedback">
-                                    Please choose a Branch.
+                                    Please choose a Dentist.
                                 </div>
                             </div>
                             <div class="mb-3 col-md-6">
-                            <label class="form-label" for="validationCustom01">Appointment Schedule</label>
-                            <select class="form-select" id="validationCustom00" required="">
+                            <label class="form-label" for="schedule_branch">Branch</label>
+                            <select class="form-select" id="schedule_branch" name="schedule_branch" required="">
                                 <option></option>
-                                <option>Anne Hathaway - July 28, 2022 8AM</option>
-                                <option>Anne Hathaway - July 28, 2022 9AM</option>
-                                <option>Lora Joy - July 28, 2022 11AM</option>
+                                <option>Commonwealth</option>
+                                <option>Novaliches</option>
+                                <option>Quezon City</option>
+                                <option>Zabarte</option>
                             </select>
-                            <!-- <input class="form-control" id="edit_appointments_sched" type="date" name="appointments_sched" required=""> -->
+                            <!-- <input class="form-control" id="validationCustom01" type="date" name="appointments_sched" required=""> -->
                             <div class="invalid-feedback">
-                                Please provide a Appointment Date.
+                                Please provide a Branch.
                             </div>
                         </div>
                         </div>            
                         
                         <div class="row g-2">
-                            <div class="mb-3 col-md-6">
-                                <label class="form-label" for="validationCustom03">Appointment Purpose</label>
-                                <input type="text" class="form-control" id="edit_appointments_purpose" placeholder="appointments_purpose" required="">
+                            <div class="col-sm-4">
+                                <label class="form-label" for="schedule_date">Schedule Date</label>
+                                <input type="date" class="form-control" id="schedule_date" name="schedule_date" placeholder="Schedule Date" required="">
                                 <div class="invalid-feedback">
-                                    Please provide an Appointment Purpose.
+                                    Please provide a Schedule Date.
                                 </div>
                             </div>
-                            <div class="mb-3 col-md-6">
-                                <label class="form-label" for="validationCustom03">Status</label>
-                                <select type="text" class="form-select" id="edit_appointments_status"  name="appointments_status" placeholder="Status" required="">
-                                    <option></option>
-                                    <option value="Approved">Approved</option>
-                                    <option value="Disapproved">Disapproved</option>
-                                    <option value="Pending">Pending</option>
-                                </select>
+                            <div class="col-sm-4">
+                            <label for="example-time" class="form-label">Start Time</label>
+                                <input class="form-control" id="schedule_start_time" type="time" name="schedule_start_time" placeholder="Start Time" required="">
                                 <div class="invalid-feedback">
-                                    Please provide a Status.
+                                    Please provide a Start Time.
+                                </div>
+                            </div>
+                            <div class="col-sm-4">
+                            <label for="example-time" class="form-label">End Time</label>
+                                <input class="form-control" id="schedule_end_time" type="time" name="schedule_end_time" placeholder="End Time" required="">
+                                <div class="invalid-feedback">
+                                    Please provide a End Time.
                                 </div>
                             </div>
                         </div>  
-
-                        <div class="row g-2">
-                                <label class="form-label" for="validationCustom04">Success</label>
-                                <select class="form-select" id="edit_appointments_success" name="appointments_success"required="">
-                                    <option></option>
-                                    <option value="Success">Yes</option>
-                                    <option value="Failed">No</option>
-                                </select>
-                                <div class="invalid-feedback">
-                                    Please choose an option.
-                                </div>
-                        </div>
-
-                        <div class="row g-2">
-                            <label class="form-label" for="validationCustom03">Comment</label>
-                            <div id="snow-editor-modify" style="height: 300px;" name="appointments_comment" required="" ></div>
-                            <div class="invalid-feedback">
-                                    Please provide a Comment.  
-                            </div>
-                        </div>
 
                         <br>
 
