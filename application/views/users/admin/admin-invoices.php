e82118b1381c2c210dccac98d7254810b17ad0e0
eurus-eastwind <86453147+eurus-eastwind@users.noreply.github.com>
2022-08-01 08:22:09 -1100

    added inovoice forms

--- application/views/users/admin/admin-invoices.php
+++ application/views/users/admin/admin-invoices.php
@@ -6,8 +6,7 @@
         <meta charset="utf-8">
         <title>Invoices | HappySmile</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta content="A fully featured admin theme which can be used to build CRM, CMS, etc." name="description">
-        <meta content="Happy Smile" name="Author">
+        <meta content="HappySmile-DCMS" name="GROUP 4" />
         <!-- App favicon -->
         <link rel="shortcut icon" href="<?php echo base_url('assets')?>/images/logos/HappySmile.ico">
 
@@ -112,6 +111,9 @@
         </div>
         <!-- END wrapper -->
 
+        <?php include ('admin-view-invoices.php');?>
+        <?php include ('admin-modify-invoice.php');?>
+        <?php include ('admin-delete-invoice.php');?>
         <!-- bundle -->
         <script src="<?php echo base_url('assets')?>/js/access/common.js"></script>
         <script src="<?php echo base_url('assets')?>/js/vendor.min.js"></script>
