30da6d21e05c8b33693251904ccd9bee1379ffa4
Jarret <Jarretbenedict@gmail.com>
2022-08-02 18:49:36 +0800

    test

--- application/views/partials/admin/account.php
+++ application/views/partials/admin/account.php
@@ -8,7 +8,7 @@
             </div> <!-- end modal header -->
             <div class="modal-body">
 
-            <form id="edit-user-form" class="needs-validation" novalidate="">
+            <form id="edit-user-form" class="needs-validation" novalidate="" enctype="multipart/form-data">
                     <input type="hidden" id="edit_users_id" name="users_id"/>
                     <div class="row g-2">
                         <div class="mb-3 col-md-6">
