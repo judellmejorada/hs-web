eff729e01720dac0ee543acf840ecf375ee59329
Jarret <Jarretbenedict@gmail.com>
2022-08-02 22:14:50 +0800

    test

--- application/views/partials/admin/account.php
+++ application/views/partials/admin/account.php
@@ -9,25 +9,105 @@
             <div class="modal-body">
                 <div class="card text-center">
                 <div class="card-body">
-                    <img src="<?php echo base_url('assets')?>/images/users/avatar-10.jpg" class="rounded-circle avatar-lg img-thumbnail" alt="profile-image">
+                    <img src="<?php echo base_url('assets')?>/images/users/avatar-10.jpg" class="rounded-circle avatar-lg img-thumbnail" id="users_profile_pic" name="users_profile_pic" alt="profile-image">
 
-                    <h4 class="mb-0 mt-2">Soeng Souy</h4>
-                    <p class="text-muted font-14">Founder</p>
+                    <h4 class="mb-0 mt-2" id="users_fullname" name="users_fullname">Soeng Souy</h4>
+                    <p class="text-muted font-14" id="users_type" name="users_type">Founder</p>
 
                     <div class="text-start mt-3">
 
-                        <p class="text-muted mb-2 font-13" id="users_fname" name="users_fname"><strong>Full Name :</strong> <span class="ms-2">Geneva D. McKnight</span></p>
+                    <form id="add-user-form" class="needs-validation" novalidate="" enctype="multipart/form-data">
+                    <div class="row g-3">
+                            <div class="col-sm-4">
+                            <label class="form-label" for="users_fname">First Name</label>
+                            <input type="text" class="form-control" id="users_fname" name="users_fname" placeholder="First Name" required="">
+                            <div class="invalid-feedback">
+                                Please provide a First Name.
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <label class="form-label" for="users_mname">Middle Name</label>
+                            <input type="text" class="form-control" id="users_mname" name="users_mname" placeholder="Middle Name" required="">
+                            <div class="invalid-feedback">
+                                Please provide a Middle Name.
+                            </div>
+                        </div>
+                        <div class="col-sm-4">
+                            <label class="form-label" for="users_lname">Last name</label>
+                            <input type="text" class="form-control" id="users_lname" name="users_lname" placeholder="Last Name" required="">
+                            <div class="invalid-feedback">
+                                Please provide a Last Name.
+                            </div>
+                        </div>
+                    </div>   
+                    
+                    <div class="row g-2">
+                        <div class="mb-3 col-md-6">
+                            <label class="form-label" for="users_birthdate">Birth Date</label>
+                            <input class="form-control" type="date" id="users_birthdate" name="users_birthdate" required="">
+                            <div class="invalid-feedback">
+                                Please provide a Birth Date.
+                            </div>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label class="form-label" for="users_gender">Gender</label>
+                            <select class="form-select" id="users_gender" name="users_gender" required="">
+                                <option></option>
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                                <option value="Prefer not to say">Prefer not to say</option>
+                                <option value="Others">Others</option>
+                            </select>
+                            <div class="invalid-feedback">
+                                Please provide a Gender.
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="row g-2">
+                        <div class="mb-3 col-md-6">
+                            <label class="form-label" for="users_civil_status">Civil Status</label>
+                            <select class="form-select" id="users_civil_status" name="users_civil_status" required="">
+                                <option></option>
+                                <option value="Single">Single</option>
+                                <option value="Married">Married</option>
+                                <option value="Divorced">Divorced</option>
+                                <option value="Others">Others</option>
+                            </select>
+                            <div class="invalid-feedback">
+                                Please provide a Civil Status.
+                            </div>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label class="form-label" for="users_email">Email</label>
+                            <input type="text" class="form-control" id="users_email" name="users_email" placeholder="Email" required="">
+                                <div class="invalid-feedback">
+                                    Please provide a Email.  
+                                </div>
+                        </div>
+                    </div>
+                                                       
+                    <div class="row g-2">
+                        <div class="mb-3 col-md-6">
+                            <label class="form-label" for="users_phone_number">Phone Number</label>
+                            <input type="text" class="form-control" id="users_phone_number" name="users_phone_number" placeholder="Phone Number" required="">
+                                <div class="invalid-feedback">
+                                    Please provide a Phone Number. 
+                                </div>
+                        </div>
+                        <div class="mb-3 col-md-6">
+                            <label class="form-label" for="users_profile_pic">Image</label>
+                            <input type="file" class="form-control" id="users_profile_pic" name="users_profile_pic" required="">
+                            <div class="invalid-feedback">
+                                    Please provide an Image. 
+                            </div>
+                        </div>
+                    </div>  
 
-                        <p class="text-muted mb-2 font-13" id="users_phone_number" name="users_phone_number"><strong>Phone Number :</strong><span class="ms-2">(123) 123 1234</span></p>
-
-                        <p class="text-muted mb-2 font-13" id="users_birthdate" name="users_birthdate"><strong>Birth Date :</strong><span class="ms-2">May 10, 1987</span></p>
-
-                        <p class="text-muted mb-2 font-13" id="users_gender" name="users_gender"><strong>Gender :</strong><span class="ms-2">Male</span></p>
-
-                        <p class="text-muted mb-2 font-13" id="users_civil_status" name="users_civil_status"><strong>Civil Status :</strong><span class="ms-2">Single</span></p>
-
-                        <p class="text-muted mb-2 font-13" id="users_email" name="users_email"><strong>Email :</strong> <span class="ms-2 ">user@email.domain</span></p>
-
+                        
+                        <div class="modal-footer">
+                    <button class="btn btn-primary" type="submit">Submit form</button>
+            </form>
                     </div>
                     
                 </div> <!-- end card-body -->
