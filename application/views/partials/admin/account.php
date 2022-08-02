b7b013fd4a8723c944fc098d2d0029262a7774be
Jarret <Jarretbenedict@gmail.com>
2022-08-02 22:05:38 +0800

    test

--- application/views/partials/admin/account.php
+++ application/views/partials/admin/account.php
@@ -14,15 +14,8 @@
                     <h4 class="mb-0 mt-2">Soeng Souy</h4>
                     <p class="text-muted font-14">Founder</p>
 
-                    <button type="button" class="btn btn-success btn-sm mb-2">Follow</button>
-                    <button type="button" class="btn btn-danger btn-sm mb-2">Message</button>
-
                     <div class="text-start mt-3">
-                        <h4 class="font-13 text-uppercase">About Me :</h4>
-                        <p class="text-muted font-13 mb-3">
-                            Hi I'm Johnathn Deo,has been the industry's standard dummy text ever since the
-                            1500s, when an unknown printer took a galley of type.
-                        </p>
+
                         <p class="text-muted mb-2 font-13" id="users_fname" name="users_fname"><strong>Full Name :</strong> <span class="ms-2">Geneva D. McKnight</span></p>
 
                         <p class="text-muted mb-2 font-13" id="users_phone_number" name="users_phone_number"><strong>Phone Number :</strong><span class="ms-2">(123) 123 1234</span></p>
@@ -35,23 +28,8 @@
 
                         <p class="text-muted mb-2 font-13" id="users_email" name="users_email"><strong>Email :</strong> <span class="ms-2 ">user@email.domain</span></p>
 
-                        <p class="text-muted mb-1 font-13" id="users_profile_pic" name="users_profile_pic"><strong>Profile Picture :</strong> <span class="ms-2">USA</span></p>
                     </div>
-
-                    <ul class="social-list list-inline mt-3 mb-0">
-                        <li class="list-inline-item">
-                            <a href="javascript: void(0);" class="social-list-item border-primary text-primary"><i class="mdi mdi-facebook"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="javascript: void(0);" class="social-list-item border-danger text-danger"><i class="mdi mdi-google"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="javascript: void(0);" class="social-list-item border-info text-info"><i class="mdi mdi-twitter"></i></a>
-                        </li>
-                        <li class="list-inline-item">
-                            <a href="javascript: void(0);" class="social-list-item border-secondary text-secondary"><i class="mdi mdi-github"></i></a>
-                        </li>
-                    </ul>
+                    
                 </div> <!-- end card-body -->
             </div> <!-- end card -->
             </div>
