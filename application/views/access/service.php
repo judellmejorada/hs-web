5448d1d02cef0ca6b819da4dfe1c789f81849a1f
Jarret <Jarretbenedict@gmail.com>
2022-08-04 02:06:36 +0800

    test

--- application/views/access/service.php
+++ application/views/access/service.php
@@ -107,8 +107,8 @@
                             <ul class="card-pricing-features">
                                <tr>
                                 <td width="70%"></td>
-                                <p class="text-justify" text-align="justify" id="services_description" name="services_description">This is by far the most common reason people visit the dentist. Many dentists recommend a cleaning every six months, and some recommend once a year. 
-                                  Either way, it’s a very important part of oral health. Your own toothbrush will never be as efficient as the tools a dentist uses to clean your teeth.
+                                <p class="text-justify" text-align="justify" id="services_description" name="services_description">This is by far the most common reason people visit the dentist. <br>Many dentists recommend a cleaning every six months, and some recommend once a year. 
+                                  Either way, it’s a very important part of oral health. Your own toothbrush will never be as efficient as the tools a dentist uses to clean your teeth.<br>
                                   An annual or semi-annual visit for a cleaning will keep your teeth healthy, shiny, and strong. Plus, a cleaning causes very little discomfort, so no need to fret!</p>
                               </tr>
                               <tr>
