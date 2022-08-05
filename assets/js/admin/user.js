684f80b14fc3b5675e5ae220d061c35d64e7baf4
Jarret <Jarretbenedict@gmail.com>
2022-08-05 21:12:52 +0800

    test

--- assets/js/admin/user.js
+++ assets/js/admin/user.js
@@ -16,6 +16,11 @@ var onDelete = (uuid) => {
 
 $(function () {
 	const dataTable = $("#products-datatable").DataTable(
+		{
+			type: "GET",
+			dom: 'Bfrtip',
+			buttons: ['copy', 'csv', 'excel', 'pdf', 'print']
+		},
 		getDataTableConfig({
 			ajax: getAjaxConfig("/admin/user/retrieve-users", {
 				type: "GET",
