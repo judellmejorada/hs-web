f0a3578fd5db1683c1d3de83923fb9ce3c770b0f
Jarret <Jarretbenedict@gmail.com>
2022-08-05 19:59:29 +0800

    test

--- application/views/users/admin/admin-users.php
+++ application/views/users/admin/admin-users.php
@@ -150,8 +150,6 @@
         <script src="<?php echo base_url('assets')?>/js/parsley.js"></script>
 
         <!-- Exportable Report css -->
-        <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
         <script src=" https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
