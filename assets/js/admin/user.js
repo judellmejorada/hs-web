ce740ef3d0a66a0379d6486b3a557b49fff0109c
Jarret <Jarretbenedict@gmail.com>
2022-08-05 22:07:33 +0800

    test

--- assets/js/admin/user.js
+++ assets/js/admin/user.js
@@ -18,17 +18,17 @@ $(function () {
 	$("#products-datatable").DataTable({
 		"processing" : true,
 		"serverSide" : true,
-		ajax : {
+		"ajax" : {
 		 type:"POST"
 		},
 		dom: 'lBfrtip',
 		buttons: [
-		 'excel', 'csv', 'pdf', 'copy'
+			'copy', 'csv', 'excel', 'pdf', 'print'
 		],
 		"lengthMenu": [ [10, 25, 50, -1], [10, 25, 50, "All"] ]
 	   });
 	   
-	  });
+	});
 	
 $(function () {
 	const dataTable = $("#products-datatable").DataTable(
