781702f8a5389f4cedb0f4a6a7844e3b17239795
Jarret <Jarretbenedict@gmail.com>
2022-08-06 12:12:50 +0800

    test

--- application/views/users/admin/admin-users.php
+++ application/views/users/admin/admin-users.php
@@ -152,7 +152,7 @@
         <script type="text/javascript" src="https://cdn.datatables.net/r/dt/jq-2.1.4,jszip-2.5.0,pdfmake-0.1.18,dt-1.10.9,af-2.0.0,b-1.0.3,b-colvis-1.0.3,b-html5-1.0.3,b-print-1.0.3,se-1.0.1/datatables.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         <script type="text/javascript" language="javascript" >
-        $(document).ready(function(){
+        $(function () {
 
         $('#products-datatable').DataTable({
         "processing" : true,
