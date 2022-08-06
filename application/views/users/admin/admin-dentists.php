ea4cfc8038887244e9adf7c9a3d85a719dd414a3
Irish M <muyright22@gmail.com>
2022-08-05 23:26:47 -0700

    button

--- application/views/users/admin/admin-dentists.php
+++ application/views/users/admin/admin-dentists.php
@@ -144,11 +144,13 @@
         <!-- third party js ends -->
         
         <!--table export-->
-        <script src="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css"></script>
-        <script src="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css"></script>
+        <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css"rel="stylesheet" >
         <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
         <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+
+        <script src="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css"></script>
         <script src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+        <script src="https://cdn.datatables.net/buttons/2.2.3/js/buttons.flash.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
