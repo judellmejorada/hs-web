2eff91a25732a7582596d42dac9ec67f01c74707
Jarret <Jarretbenedict@gmail.com>
2022-08-06 12:14:40 +0800

    test

--- application/views/users/admin/admin-users.php
+++ application/views/users/admin/admin-users.php
@@ -152,14 +152,14 @@
         <script type="text/javascript" src="https://cdn.datatables.net/r/dt/jq-2.1.4,jszip-2.5.0,pdfmake-0.1.18,dt-1.10.9,af-2.0.0,b-1.0.3,b-colvis-1.0.3,b-html5-1.0.3,b-print-1.0.3,se-1.0.1/datatables.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
         <script type="text/javascript" language="javascript" >
-        $(function () {
+        $(document).ready(function(){
 
         $('#products-datatable').DataTable({
         "processing" : true,
         "serverSide" : true,
-        ajax: getAjaxConfig("/admin/user/retrieve-users", {
-				type: "POST",
-			}),
+        "ajax" : {
+            type:"POST"
+        },
         dom: 'lBfrtip',
         buttons: [
             'excel', 'csv', 'pdf', 'copy'
