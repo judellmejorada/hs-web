13b230d4b40f1a13064b818db56c69012e47fbc9
Irish M <muyright22@gmail.com>
2022-08-06 13:47:31 -0700

    modified

--- application/views/users/admin/admin-dentists.php
+++ application/views/users/admin/admin-dentists.php
@@ -147,8 +147,6 @@
         <link href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css"rel="stylesheet" style="text/css" >
         <link href="https://cdn.datatables.net/buttons/2.2.3/css/buttons.dataTables.min.css"rel="stylesheet" style="text/css" >
 
-        <script type="text/javascript" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-        <script type="text/javascript" src="https://cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
         <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
