bb79ac4720d4a58698703fb7b2219bf8e6009a85
Irish M <muyright22@gmail.com>
2022-08-05 21:36:54 -0700

    Modified buttons

--- application/views/users/admin/admin-branches.php
+++ application/views/users/admin/admin-branches.php
@@ -132,7 +132,17 @@
         <script src="<?php echo base_url('assets')?>/js/vendor/responsive.bootstrap5.min.js"></script>
         <script src="<?php echo base_url('assets')?>/js/vendor/dataTables.checkboxes.min.js"></script>
         <!-- third party js ends -->
-
+        
+        <!--table export-->
+        <script src="//code.jquery.com/jquery-3.5.1.js"></script>
+        <script src="//cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+        <script src="//cdn.datatables.net/buttons/2.2.3/js/dataTables.buttons.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+        <script src="//cdn.datatables.net/buttons/2.2.3/js/buttons.html5.min.js"></script>
+        <script src="//cdn.datatables.net/buttons/2.2.3/js/buttons.print.min.js"></script>
+        
         <!-- demo app -->
         <script src="<?php echo base_url('assets')?>/js/admin/branch.js"></script>
         <script src="<?php echo base_url('assets')?>/js/pages/demo.branches.js"></script>
