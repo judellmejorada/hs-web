de933699d20313921363becf2b7980efb6a5026b
irish m <muyright22@gmail.com>
2022-08-07 21:48:52 -0700

    dentist

--- application/views/partials/dentist/left_sidebar.php
+++ application/views/partials/dentist/left_sidebar.php
@@ -28,11 +28,7 @@
                         <li class="side-nav-title side-nav-item">DENTIST</li>
 
 
-                        <li class="side-nav-item">
-                            <a data-bs-toggle="" href="<?php echo base_url('dentist/appointment')?>" class="side-nav-link">
-                                <i class="uil-briefcase-alt"></i>
-                                <span> Appointments </span>
-                            </a>
+                        
 
                             <a data-bs-toggle="" href="<?php echo base_url('dentist/calendar')?>" class="side-nav-link">
                                 <i class="uil-calendar-alt"></i>
